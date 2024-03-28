4dcadf7a819ef293acb6fc9df7cbcc0425fa31a3
JosephK2024 <josephstonekennedy.2024@gmail.com>
2024-03-28 10:44:25 -0400

    Base for collab on practice project

--- CSP_Practice_Project/program.py
+++ CSP_Practice_Project/program.py
@@ -1,1 +1,26 @@
-#placeholder
+# Basketball thing
+# coded by: jsk & hk
+import turtle
+import  time 
+
+#Turtle tester
+placeholder = turtle.Turtle()
+
+# Setup screen
+window = turtle.Screen()
+window.setup(1000, 1000)
+
+#add object types to screen
+ball = "ball.gif"
+hoop = "hoop.gif"
+window.addshape(ball)
+window.addshape(hoop)
+
+#-----Global Variables-----
+valid_angles = [35, 45, 55, 65]
+
+#for testing
+placeholder.shape(ball)
+placeholder.shapesize(0.0001)
+
+window.mainloop()
